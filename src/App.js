e1278258b09a92f1d614d1eb0f978897195d0784
Cristovão Luiz <cristovoxdgm@gmail.com>
2018-08-08 05:54:15 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  
+
   render() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+   
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+         
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -16,6 +27,8 @@ class App extends Component {
       </div>
     );
   }
+  
 }
+ 
 
 export default App;
